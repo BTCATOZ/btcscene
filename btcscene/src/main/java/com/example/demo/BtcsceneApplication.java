122d96c19b96e6d3fa9b3e68b6c05dd1f857a291
btcscene <btcscene@gmail.com>
2023-02-14 15:16:09 +0900

    test

---
+++ btcscene/src/main/java/com/example/demo/BtcsceneApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcsceneApplication {
+
+	public static void main(String[] args) {
+		
+		
+		SpringApplication.run(BtcsceneApplication.class, args);
+	}
+
+}
